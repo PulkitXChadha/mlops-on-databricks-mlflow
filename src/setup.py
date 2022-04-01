2fa743bdfb8af430b36d7a8044374ce2362af611
Pulkit Chadha <pulkit2304@gmail.com>
2022-04-01 14:55:23 -0400

    added setup tool for libraries

---
+++ src/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="mlops_on_databricks_mlflow",
+    version="0.0.1",
+    author="Pulkit Chadha",
+    author_email="",
+    description="MLOps on Databricks and MLflow helpers",
+    long_description_content_type="text/markdown",
+    url="https://github.com/PulkitXChadha/mlops-on-databricks-mlflow.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/PulkitXChadha/mlops-on-databricks-mlflow.git/issues",
+    },
+    install_requires=[],
+    packages=find_packages(exclude=('tests*', 'testing*', 'docs')),
+    entry_points={
+        'console_scripts': [
+            'post_comment_cli=mlops_on_databricks_mlflow.mlflowAPIWrapper:postComment',
+        ],
+    }
+)
