10fb3a5c1e241f89226aea961fbddab0e235911d
Pulkit Chadha <33003354+PulkitXChadha@users.noreply.github.com>
2022-04-01 13:30:19 -0400

    Create mlflowAPIWrapper.py

---
+++ mlflowAPIWrapper.py
@@ -0,0 +1,17 @@
+import mlflow
+import json
+from mlflow.utils.rest_utils import http_request
+from mlflow.tracking import MlflowClient
+
+client = MlflowClient()
+host_creds = client._tracking_client.store.get_host_creds()
+ 
+def mlflow_call_endpoint(endpoint, method, body='{}'):
+  if method == 'GET':
+      response = http_request(host_creds=host_creds, endpoint="/api/2.0/mlflow/{}".format(endpoint), method=method, params=json.loads(body))
+  else:
+      response = http_request(host_creds=host_creds, endpoint="/api/2.0/mlflow/{}".format(endpoint), method=method, json=json.loads(body))
+  return response.json()
+ 
+def postComment(json_obj):
+  mlflow_call_endpoint('comments/create', 'POST', body=json.dumps(json_obj))
