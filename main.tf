74cf15c92328a4ad531dbc2fd67eeb96df8fe217
Pulkit Chadha <pulkit2304@gmail.com>
2022-04-04 11:41:19 -0400

    updated terraform

--- main.tf
+++ main.tf
@@ -14,28 +14,14 @@ terraform {
   }
 }
 
-
-
 provider "databricks" {
-  # Configuration options
-  # host  = var.DATABRICKS_HOST
-  # token = var.DATABRICKS_TOKEN
 }
-#variable "DATABRICKS_WORKSPACE_URL" {
-#  type        = string
-#  description = "This is the PAT token"
-#}
-#variable "DATABRICKS_TOKEN" {
-#  type        = string
-#  description = "This is the PAT token"
-#}
 
 data "databricks_current_user" "me" {}
 data "databricks_spark_version" "latest" {}
 data "databricks_node_type" "smallest" {
   local_disk = true
 }
-
 
 resource "databricks_notebook" "this" {
   path     = "${data.databricks_current_user.me.home}/Terraform"
@@ -49,20 +35,16 @@ resource "databricks_notebook" "this" {
 
 resource "databricks_job" "this" {
   name = "Terraform Demo 2.0 (${data.databricks_current_user.me.alphanumeric})"
-
   new_cluster {
     num_workers   = 1
     spark_version = data.databricks_spark_version.latest.id
     node_type_id  = data.databricks_node_type.smallest.id
   }
-
   notebook_task {
     notebook_path = databricks_notebook.this.path
   }
-
   email_notifications {}
 }
-
 output "notebook_url" {
   value = databricks_notebook.this.url
 }
